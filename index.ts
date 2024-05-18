d1962aec325489da2bfb568881c20b021dd7a760
TosuSW <denisappleid12@gmail.com>
2024-05-18 10:40:36 +0300

    Getting the title of a game

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import * as puppeteer from 'puppeteer';
 
-const url = 'https://store.steampowered.com/category/action/'
+const url = 'https://store.steampowered.com/app/227300/Euro_Truck_Simulator_2/'
 
 const runMain = async () => {
-    const browser  = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto(url)
+
+    const title = await page.$eval('.apphub_AppName', el => el.innerHTML)
+
+
+    console.log('The Title: ' + title)
 
     browser.close()
 }
