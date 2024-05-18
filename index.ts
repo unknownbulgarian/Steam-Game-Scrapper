2b51ed257af032da3c7b4ba484af88e822b58f61
TosuSW <denisappleid12@gmail.com>
2024-05-18 10:59:03 +0300

    Getting the minimum Requirements

--- index.ts
+++ index.ts
@@ -8,9 +8,18 @@ const runMain = async () => {
     await page.goto(url)
 
     const title = await page.$eval('.apphub_AppName', el => el.innerHTML)
-
+    
+    const minRequirements = await page.$eval('.game_area_sys_req_leftCol', el => {
+        const items = el.querySelector('.bb_ul')?.querySelectorAll('li');
+        if(!items) {
+            return [];
+        }
+        return Array.from(items, item => item.textContent?.trim());
+    });
+   
 
     console.log('The Title: ' + title)
+    console.log(minRequirements)
 
     browser.close()
 }
