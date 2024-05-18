0e752c12263b308abb37ffe34adec13e87ad6408
TosuSW <denisappleid12@gmail.com>
2024-05-18 11:01:40 +0300

    Getting the recommendedRequirements

--- index.ts
+++ index.ts
@@ -8,18 +8,27 @@ const runMain = async () => {
     await page.goto(url)
 
     const title = await page.$eval('.apphub_AppName', el => el.innerHTML)
-    
+
     const minRequirements = await page.$eval('.game_area_sys_req_leftCol', el => {
         const items = el.querySelector('.bb_ul')?.querySelectorAll('li');
-        if(!items) {
+        if (!items) {
             return [];
         }
         return Array.from(items, item => item.textContent?.trim());
     });
-   
+
+    const recommendedRequirements = await page.$eval('.game_area_sys_req_rightCol', el => {
+        const items = el.querySelector('.bb_ul')?.querySelectorAll('li');
+        if (!items) {
+            return [];
+        }
+        return Array.from(items, item => item.textContent?.trim());
+    })
+
 
     console.log('The Title: ' + title)
     console.log(minRequirements)
+    console.log(recommendedRequirements)
 
     browser.close()
 }
